2d5c026a4a4f94deb90f3d4d57da72f5f65bcacf
Randhal Ramirez <randhalramirez@gmail.com>
2023-02-23 17:01:55 -0600

    Adding a test implementation of the custom library

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
-import { HelloWorld, InputBox } from 'sb-components-take-two';
+import {CustomButton, HelloWorld, InputBox} from 'sb-components-take-two';
 
 function App() {
   return (
     <div className="App">
       <HelloWorld />
       <InputBox />
+        <CustomButton variant='contained' innerText={'Hey Hey Hey'} onClick={() => alert('It works!')} />
     </div>
   );
 }
